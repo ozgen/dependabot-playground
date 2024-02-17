ee1eb85c79ade957449a56efc2030f14ce84b020
ozgen <ozgn45@gmail.com>
2024-02-17 16:35:20 +0100

    add init

---
+++ src/test/java/com/ozgen/depandablebotplayground/DepandableBotPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozgen.depandablebotplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepandableBotPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
