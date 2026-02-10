1f66905af5b21ea92cf1234cf7138a9a6c5b59cf
ozgen <ozgn45@gmail.com>
2026-02-10 15:42:25 +0100

    add test comment

--- src/main/java/com/ozgen/depandablebotplayground/DepandableBotPlaygroundApplication.java
+++ src/main/java/com/ozgen/depandablebotplayground/DepandableBotPlaygroundApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DepandableBotPlaygroundApplication {
 
     public static void main(String[] args) {
-        System.out.println("hello test");
+        System.out.println("hello test test");
         SpringApplication.run(DepandableBotPlaygroundApplication.class, args);
     }
 
